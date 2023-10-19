99ac4c03b367a7933db468e9dd3d00df1a9b762c
Elianne Márcia MUdrei <101529296+ProfessoraEliane@users.noreply.github.com>
2023-10-19 19:41:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ function tocaSomPom () {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSomPom;
+
+function tocaSomRisada(){
+    document.querySelector("#som_tecla_risada").play();
+    
+}
+const listadeTeclas = document.querySelectorAll(".tecla");
+listadeTeclas[2].onclick = tocaSomRisada;
