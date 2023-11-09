f3f2f5ed5f56cd1d264e713e608ea947acf26da7
Elianne Márcia MUdrei <101529296+ProfessoraEliane@users.noreply.github.com>
2023-11-09 18:50:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
+
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function(){
+    const tecla =  listaDeTeclas[contador];  
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function(){
         tocaSom(idAudio);
     }
     contador = contador + 1;
-   
 }
